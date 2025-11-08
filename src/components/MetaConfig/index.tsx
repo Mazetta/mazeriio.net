291f97a7a978ee76571b077e7c47d3a9a622910e
Mazeriio <110097914+Mazetta@users.noreply.github.com>
2025-11-08 19:33:56 +0000

    a new test, again

--- src/components/MetaConfig/index.tsx
+++ src/components/MetaConfig/index.tsx
@@ -19,16 +19,16 @@ const MetaConfig: React.FC<MetaConfigProps> = (props) => {
       <meta name="description" content={props.description} />
       {/* og */}
       <meta property="og:type" content={props.type} />
-      <meta property="og:title" content={props.title} />
-      <meta property="og:description" content={props.description} />
-      <meta property="og:url" content={props.url} />
+      <meta property="og:title" content="test" />
+      <meta property="og:description" content="test" />
+      <meta property="og:url" content="mazeriio.net" />
       {CONFIG.lang && <meta property="og:locale" content={CONFIG.lang} />}
-      {props.image && <meta property="og:image" content={props.image} />}
+      {props.image && <meta property="og:image" content="https://www.mazeriio.net/icon-128.png" />}
       {/* twitter */}
       <meta name="twitter:title" content={props.title} />
-      <meta name="twitter:description" content={props.description} />
+      <meta name="twitter:description" content="test" />
       <meta name="twitter:card" content="summary_large_image" />
-      {props.image && <meta name="twitter:image" content={props.image} />}
+      {props.image && <meta name="twitter:image" content="https://www.mazeriio.net/icon-128.png" />}
       {/* post */}
       {props.type === "Post" && (
         <>
