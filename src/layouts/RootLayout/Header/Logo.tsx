f64ab6199e10c3bcf507002499d274e40e0cfa36
Mazeriio <110097914+Mazetta@users.noreply.github.com>
2025-11-09 19:16:05 +0000

    test: misalignment again

--- src/layouts/RootLayout/Header/Logo.tsx
+++ src/layouts/RootLayout/Header/Logo.tsx
@@ -7,17 +7,40 @@ const Logo = () => {
   return (
     <StyledWrapper href="/" aria-label={CONFIG.blog.title}>
       <IconWrapper>
-        <Image src={CONFIG.blog.icon} alt="" width={16} height={16} />
-      </IconWrapper> 
-      {CONFIG.blog.title}
+        <Image
+          src={CONFIG.blog.icon}
+          alt="Logo icon"
+          width={24}
+          height={24}
+          style={{ objectFit: "contain" }}
+        />
+      </IconWrapper>
+      <Title>{CONFIG.blog.title}</Title>
     </StyledWrapper>
   )
 }
 
 export default Logo
+
+const StyledWrapper = styled(Link)`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+  text-decoration: none;
+  font-weight: 600;
+  font-size: 1.1rem;
+  line-height: 1;
+`
+
 const IconWrapper = styled.span`
-  display: inline-flex;
+  display: flex;
   align-items: center;
   justify-content: center;
+  height: 1.5rem;
 `
-const StyledWrapper = styled(Link)``
+
+const Title = styled.span`
+  display: flex;
+  align-items: center;
+  line-height: 1;
+`
