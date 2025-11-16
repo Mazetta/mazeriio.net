b6ffe628581c398d3d6c898e7cae02df7d170d9e
Mazeriio <110097914+Mazetta@users.noreply.github.com>
2025-11-16 15:40:57 +0000

    fix: removal of previous modules

--- src/routes/Detail/PostDetail/CommentBox/index.tsx
+++ src/routes/Detail/PostDetail/CommentBox/index.tsx
@@ -3,19 +3,6 @@ import { CONFIG } from "site.config"
 import dynamic from "next/dynamic"
 import GiscusComments from "src/components/GiscusComments"
 
-const UtterancesComponent = dynamic(
-  () => {
-    return import("./Utterances")
-  },
-  { ssr: false }
-)
-const CusdisComponent = dynamic(
-  () => {
-    return import("./Cusdis")
-  },
-  { ssr: false }
-)
-
 type Props = {
   data: TPost
 }
