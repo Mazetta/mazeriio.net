d118f26f3f728f50651b3c0a99c8bb79f3527b03
Mazeriio <110097914+Mazetta@users.noreply.github.com>
2025-11-16 00:35:33 +0000

    test

--- src/components/GiscusComments.tsx
+++ src/components/GiscusComments.tsx
@@ -1,46 +1,22 @@
-import Giscus from "@giscus/react"
-import useScheme from "src/hooks/useScheme"
-import { useEffect } from "react"
-import styled from "@emotion/styled"
-import { useRouter } from "next/router"
+import Giscus from '@giscus/react';
+import useScheme from 'src/hooks/useScheme';
 
-const GiscusComments: React.FC = () => {
-  const [scheme] = useScheme()
-  const router = useRouter()
-
-  useEffect(() => {
-    const theme = `github-${scheme}`
-    const script = document.createElement("script")
-    const anchor = document.getElementById("comments")
-    if (!anchor) return
-      script.setAttribute("src", "https://giscus.app/client.js")
-      script.setAttribute("repo", "Mazetta/mazeriio.net")
-      script.setAttribute("repoid", "R_kgDOQQyKPQ")
-      script.setAttribute("category", "Comments")
-      script.setAttribute("categoryid", "DIC_kwDOQQyKPc4Cx1DI")
-      script.setAttribute("mapping", "og:title")
-      script.setAttribute("reactionsenabled", "1")
-      script.setAttribute("emitmetadata", "0")
-      script.setAttribute("inputposition", "top")
-      script.setAttribute("theme", "dark")
-      script.setAttribute("loading", "lazy")
-      script.setAttribute("lang", "en")
-      script.setAttribute("crossorigin", "anonymous")
-      script.setAttribute("async", `true`)
-      anchor.appendChild(script)
-    return () => {
-      anchor.innerHTML = ""
-    }
-  }, [scheme, router])
+export default function GiscusComments() {
   return (
-    <>
-      <StyledWrapper id="comments">
-        <div className="giscus-frame"></div>
-      </StyledWrapper>
-    </>
-  )
+    <Giscus
+      id="comments"
+      repo="Mazetta/mazeriio.net"
+      repoId="R_kgDOQQyKPQ"
+      category="Comments"
+      categoryId="DIC_kwDOQQyKPc4Cx1DI"
+      mapping="og:title"
+      term="Giscus Comments"
+      reactionsEnabled="1"
+      emitMetadata="0"
+      inputPosition="top"
+      theme="transparent_dark"
+      lang="en"
+      loading="lazy"
+    />
+  );
 }
-
-export default GiscusComments
-
-const StyledWrapper = styled.div``
