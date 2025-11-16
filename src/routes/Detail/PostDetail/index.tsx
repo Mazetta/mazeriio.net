a9c280f4f766d388bd8587e274e2f55dcdd8fbdf
Mazeriio <110097914+Mazetta@users.noreply.github.com>
2025-11-16 12:29:04 +0000

    style: moved share label

--- src/routes/Detail/PostDetail/index.tsx
+++ src/routes/Detail/PostDetail/index.tsx
@@ -61,9 +61,9 @@ const PostDetail: React.FC<Props> = () => {
         <div>
           <NotionRenderer recordMap={data.recordMap} />
         </div>
-
+        
+        <div className="label">Share:</div>
         <ShareSection>
-          <div className="label">Share:</div>
           <TooltipWrapper>
             <span>Share on Bluesky</span>
             <BlueskyShareButton url={postUrl} title={title}>
